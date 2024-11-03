c130b6e2a138f6f8d4e28dd8f1b153d795448cc5
Vincent Velasco <90242180+Vincent-Velasco@users.noreply.github.com>
2024-11-03 21:35:27 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
 
+st.title("Welcome to my Dicoding Analisa Dashboard Website")
+st.header("My name is Vincent Velasco")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    
