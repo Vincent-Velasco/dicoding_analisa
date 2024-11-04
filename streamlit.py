ddaa1256d100f6d9e095e3351a9ecdfd90c880ec
Vincent Velasco <90242180+Vincent-Velasco@users.noreply.github.com>
2024-11-04 15:02:46 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -20,25 +20,22 @@ with col1:
 
     bike_df_day_group = bike_df_day.groupby("workingday").mean()
     
-    # bar_width = 0.3
+    bar_width = 0.3
     
-    # x = np.arange(len(bike_df_day_group.index))
+    x = np.arange(len(bike_df_day_group.index))
     
-    # x2 = [i + bar_width for i in x]
+    x2 = [i + bar_width for i in x]
     
-    # fig = plt.figure(figsize = (15,10))
-    # plt.bar(x,bike_df_day_group["casual"], label = "Casual", width = bar_width)
-    # plt.bar(x2,bike_df_day_group["registered"], label = "Registered", width = bar_width)
+    fig = plt.figure(figsize = (15,10))
+    plt.bar(x,bike_df_day_group["casual"], label = "Casual", width = bar_width)
+    plt.bar(x2,bike_df_day_group["registered"], label = "Registered", width = bar_width)
     
-    # plt.xticks([0,1], ["Weekend", "Weekday"])
-    # plt.xlabel("Working Day or Not")
-    # plt.ylabel("Average Rental Bike")
-    # plt.title("Distribution of Casual and Registered User in Working Day or Not")
-    # plt.legend()
-    # plt.show()
-
-    # st.write("Table group by working day")
-    # st.table(bike_df_day_group)
+    plt.xticks([0,1], ["Weekend", "Weekday"])
+    plt.xlabel("Working Day or Not")
+    plt.ylabel("Average Rental Bike")
+    plt.title("Distribution of Casual and Registered User in Working Day or Not")
+    plt.legend()
+    plt.show()
 
     st.write("Conclusion pertanyaan 1:Kurang lebih tidak ada hubungan antara jumlah rental sepeda dengan hari kerja atau tidak, tetapi bisa dilihat bahwa di hari kerja, jumlah user yang registered signifikan lebih banyak dibandingkan casual user, sehingga bisa dikatakan bahwa banyak user yang menggunakan rental sepeda untuk berangkat kerja")
 
@@ -46,13 +43,13 @@ with col1:
 with col2:
     st.write("Pertanyaan 2: Apakah ada peningkatan dari rental sepeda selama 2 tahun berjalan?")
 
-    # bike_df_month = bike_df[["dteday", "casual", "registered", "cnt"]]
+    bike_df_month = bike_df[["dteday", "casual", "registered", "cnt"]]
     
-    # bike_df_month["dteday"] = pd.to_datetime(bike_df_month["dteday"])
+    bike_df_month["dteday"] = pd.to_datetime(bike_df_month["dteday"])
     
-    # bike_df_month = bike_df_month.groupby(pd.Grouper(key = "dteday", freq = 'M')).sum()
+    bike_df_month = bike_df_month.groupby(pd.Grouper(key = "dteday", freq = 'M')).sum()
     
-    # st.write("Line Chart by Month")
-    # st.line_chart(bike_df_month)
+    st.write("Line Chart by Month")
+    st.line_chart(bike_df_month)
 
     st.write("Conclusion pertanyaan 2: Di setiap tahunnya mengalami peningkatan jumlah rental sepeda (sehingga bisa untuk menambahkan lokasi atau ekspansi rental sepeda), tetapi pada awal dan akhir tahun akan mengalami penuruan dalam jumlah rental sepeda dan juga peak dari rental sepeda berada di tengah tahun")
